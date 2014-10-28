e5a481107e5acee89be44e93e60142a09e4dfc20
David Ganey <dhganey@gmail.com>
2014-10-27 18:26:00 -0700

    Add initial VC

---
+++ MainTipViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MainTipViewController.swift
+//  460-tip-app
+//
+//  Created by David Ganey on 10/27/14.
+//  Copyright (c) 2014 dhganey. All rights reserved.
+//
+
+import UIKit
+
+class MainTipViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        NSLog("testing")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
