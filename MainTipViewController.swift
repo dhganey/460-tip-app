02804d4dd5043e38ab9bd7dbfa29a3ce763a4e5c
David Ganey <dhganey@gmail.com>
2014-10-27 18:38:24 -0700

    Set up view controller. Tool bar does not work

--- MainTipViewController.swift
+++ MainTipViewController.swift
@@ -10,14 +10,17 @@ import UIKit
 
 class MainTipViewController: UIViewController {
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         NSLog("testing")
+        self.navigationController?.setToolbarHidden(false, animated: false) //todo no toolbar
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
