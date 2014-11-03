e448b0263f68bfa0877951c1b7219741f5aafaea
David Ganey <dhganey@gmail.com>
2014-11-03 16:48:02 -0700

    Adjust switches on config screen

--- 460-tip-app/TipConfigViewController.swift
+++ 460-tip-app/TipConfigViewController.swift
@@ -14,6 +14,8 @@ class TipConfigViewController: UIViewController
     
     @IBOutlet weak var minField: UITextField!
     @IBOutlet weak var maxField: UITextField!
+    @IBOutlet weak var tipOnTaxSwitch: UISwitch!
+    @IBOutlet weak var tipOnDeductionsSwitch: UISwitch!
 
     required init(coder aDecoder: NSCoder)
     {
@@ -26,6 +28,9 @@ class TipConfigViewController: UIViewController
         
         minField.text = NSString(format: "%.2f", self.model!.minTipPercent * 100.0)
         maxField.text = NSString(format: "%.2f", self.model!.maxTipPercent * 100.0)
+        
+        self.tipOnTaxSwitch.on = self.model!.tipOnTax
+        self.tipOnDeductionsSwitch.on = self.model!.tipOnDeductions
     }
 
     override func didReceiveMemoryWarning()
@@ -35,11 +40,21 @@ class TipConfigViewController: UIViewController
     
     @IBAction func minEdited(sender: AnyObject)
     {
-        
+        self.model!.minTipPercent = NSString(format: "%@", self.minField.text).doubleValue / 100.0
     }
     
     @IBAction func maxEdited(sender: AnyObject)
     {
-        
+        self.model!.maxTipPercent = NSString(format: "%@", self.maxField.text).doubleValue / 100.0
+    }
+    
+    @IBAction func tipOnTaxToggled(sender: AnyObject)
+    {
+        self.model!.tipOnTax = !self.model!.tipOnTax
+    }
+    
+    @IBAction func tipOnDeductionsToggled(sender: AnyObject)
+    {
+        self.model!.tipOnDeductions = !self.model!.tipOnDeductions
     }
 }
