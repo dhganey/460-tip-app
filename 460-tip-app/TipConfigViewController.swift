79fdd5d1ebaeee6469c6fe59494ca8a22f0ec903
David Ganey <dhganey@gmail.com>
2014-11-06 18:47:23 -0700

    Add error handling to config tip min/max fields

--- 460-tip-app/TipConfigViewController.swift
+++ 460-tip-app/TipConfigViewController.swift
@@ -11,9 +11,11 @@ import UIKit
 class TipConfigViewController: UIViewController
 {
     var model: TipDataObjectModel?
+
     
-    @IBOutlet weak var minField: UITextField!
+
     @IBOutlet weak var maxField: UITextField!
+    @IBOutlet weak var minField: UITextField!
     @IBOutlet weak var tipOnTaxSwitch: UISwitch!
     @IBOutlet weak var tipOnDeductionsSwitch: UISwitch!
 
@@ -40,12 +42,80 @@ class TipConfigViewController: UIViewController
     
     @IBAction func minEdited(sender: AnyObject)
     {
-        self.model!.minTipPercent = NSString(format: "%@", self.minField.text).doubleValue / 100.0
+        let minStr: NSString = self.minField.text
+        let newMin = minStr.doubleValue / 100.0
+        
+        if (newMin > self.model!.maxTipPercent) //min > max not allowed
+        {
+            let alertController = UIAlertController(title: "Error", message: "Min tip cannot be lower than max", preferredStyle: UIAlertControllerStyle.Alert)
+            let OKaction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: {
+                (action) in alertController.dismissViewControllerAnimated(true, completion: nil)
+            })
+            alertController.addAction(OKaction)
+            
+            self.presentViewController(alertController, animated: true, completion: nil)
+            
+            //reset to model
+            self.minField.text = NSString(format: "%.2f", (self.model!.minTipPercent * 100.0))
+        }
+        else if (newMin < 0) //min < 0 not allowed
+        {
+            let alertController = UIAlertController(title: "Error", message: "Min tip cannot be lower than 0", preferredStyle: UIAlertControllerStyle.Alert)
+            let OKaction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: {
+                (action) in alertController.dismissViewControllerAnimated(true, completion: nil)
+            })
+            alertController.addAction(OKaction)
+            
+            self.presentViewController(alertController, animated: true, completion: nil)
+            
+            //reset to model
+            self.minField.text = NSString(format: "%.2f", (self.model!.minTipPercent * 100.0))
+        }
+        else //good to go
+        {
+            self.model!.minTipPercent = newMin
+            self.minField.text = NSString(format: "%.2f", (self.model!.minTipPercent * 100.0))
+        }
     }
     
     @IBAction func maxEdited(sender: AnyObject)
     {
-        self.model!.maxTipPercent = NSString(format: "%@", self.maxField.text).doubleValue / 100.0
+        let maxStr: NSString = self.maxField.text
+        let newMax = maxStr.doubleValue / 100.0
+        
+        if (newMax < self.model!.minTipPercent) //max < min not allowed
+        {
+            let alertController = UIAlertController(title: "Error", message: "Max tip cannot be lower than min", preferredStyle: UIAlertControllerStyle.Alert)
+            let OKaction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: {
+                (action) in alertController.dismissViewControllerAnimated(true, completion: nil)
+            })
+            alertController.addAction(OKaction)
+            
+            self.presentViewController(alertController, animated: true, completion: nil)
+            
+            //reset to model
+            self.maxField.text = NSString(format: "%.2f", (self.model!.maxTipPercent * 100.0))
+        }
+        else if (newMax < 0) //min < 0 not allowed
+        {
+            let alertController = UIAlertController(title: "Error", message: "Max tip cannot be lower than 0", preferredStyle: UIAlertControllerStyle.Alert)
+            let OKaction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: {
+                (action) in alertController.dismissViewControllerAnimated(true, completion: nil)
+            })
+            alertController.addAction(OKaction)
+            
+            self.presentViewController(alertController, animated: true, completion: nil)
+            
+            //reset to model
+            self.maxField.text = NSString(format: "%.2f", (self.model!.maxTipPercent * 100.0))
+        }
+        else //good to go
+        {
+            self.model!.maxTipPercent = newMax
+            self.maxField.text = NSString(format: "%.2f", (self.model!.maxTipPercent * 100.0))
+
+        }
+
     }
     
     @IBAction func tipOnTaxToggled(sender: AnyObject)
